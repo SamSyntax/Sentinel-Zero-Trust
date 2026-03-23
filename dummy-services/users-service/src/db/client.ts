12b45208fb8ba9c23845609e5284f6c251f207e7
SamSyntax <klfracc@gmail.com>
2026-03-23 22:52:06 +0100

    Feat: creating drizzle schemas and client

---
+++ dummy-services/users-service/src/db/client.ts
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+import * as schema from "./schema/schema"
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres'
+
+const client = postgres(process.env.DATABASE_URL!);
+export const db = drizzle(client, { schema: schema });
+
+
+
