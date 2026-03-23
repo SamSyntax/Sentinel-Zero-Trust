43d399e523bb9d886a4a9687c54d38c17c9ebffc
SamSyntax <klfracc@gmail.com>
2026-03-23 22:51:42 +0100

    Feat: adding full crud to the dummy service

--- dummy-services/users-service/src/handlers/users.ts
+++ dummy-services/users-service/src/handlers/users.ts
@@ -1,45 +1,71 @@
 import type { FastifyReply, FastifyRequest } from "fastify";
 import type { IdParam, User } from "../schemas/user";
+import { db } from "../db/client";
+import { usersTable } from "../db/schema/schema";
+import { eq } from "drizzle-orm";
 
 
-const users: User[] = [
-  { id: 1, name: "John", email: "john@example.com" },
-  { id: 2, name: "Jane", email: "jane@example.com" },
-  { id: 3, name: "Bob", email: "bob@example.com" },
-  { id: 4, name: "Alice", email: "alice@example.com" },
-  { id: 5, name: "Charlie", email: "charlie@example.com" },
-  { id: 6, name: "Dave", email: "dave@example.com" },
-  { id: 7, name: "Eve", email: "eve@example.com" },
-  { id: 8, name: "Frank", email: "frank@example.com" },
-];
-
-
-export function getUsers(req: FastifyRequest, reply: FastifyReply) {
-  req.log.info("getUsers hit");
+export async function getUsers(_: FastifyRequest, reply: FastifyReply) {
+  const users = await db.query.usersTable.findMany();
   reply.send(users).status(200);
 }
 
-export function getSingleUser(req: FastifyRequest<{ Params: IdParam }>, reply: FastifyReply) {
-  req.log.info("getSingleUser hit");
+export async function getSingleUser(req: FastifyRequest<{ Params: IdParam }>, reply: FastifyReply) {
   const { id } = req.params;
-  const user = users.find((user) => user.id === Number(id));
+
+  const user = await db.query.usersTable.findFirst({
+    where: eq(usersTable.id, Number(id))
+  })
+
   if (!user) {
     return reply.status(404).send({ message: "User not found" });
   }
   reply.send(user).status(200);
 }
 
-export function createUser(req: FastifyRequest<{ Body: User }>, reply: FastifyReply) {
+export async function createUser(req: FastifyRequest<{ Body: User }>, reply: FastifyReply) {
   try {
-    const { email, name } = req.body;
-    const user: User = {
-      id: users.length + 1,
+    const { email, name, age } = req.body;
+    console.log(email, name, age)
+    const user = await db.insert(usersTable).values({
+      email,
       name,
-      email
-    }
-    users.push(user);
+      age
+    }).returning();
+
     reply.send(user).status(201);
   } catch (err: any) {
     reply.status(400).send({ message: err.message });
+  }
+}
+
+export async function updateUser(req: FastifyRequest<{ Body: User, Params: IdParam }>, reply: FastifyReply) {
+  try {
+    const { email, name, age } = req.body;
+    console.log(email, name, age)
+    const user = await db.update(usersTable).set({
+      email,
+      name,
+      age
+    }).where(eq(usersTable.id, Number(req.params.id)))
+
+    reply.send(user).status(204);
+  }
+
+  catch (err: any) {
+    reply.status(400).send({ message: err.message });
+  }
+}
+
+export async function deleteUser(req: FastifyRequest<{ Params: IdParam }>, reply: FastifyReply) {
+  try {
+    const user = await db.delete(usersTable).where(eq(usersTable.id, Number(req.params.id))).returning();
+    if (!user || user.length === 0) {
+      return reply.status(204).send({ message: "User not found" });
+    }
+    reply.send().status(200)
+
+  } catch (err: any) {
+    reply.status(500).send({ message: err.message });
   }
 }
